36d3fdb1ce9fb36aa47154683b28f3a1a6c12b74
Darren M <aeneascorrupt@gmail.com>
2017-06-01 17:36:28 -0700

    patch: increase timeout
    By default, mocha will time out after 2 seconds. This may be too
    short of a run, as it is latency-dependent. This change aims to
    increase the timeout to a reasonable 30 seconds: long enough for
    the test to accomplish what it's supposed to and short enough that
    it should timeout when there are other factors involved

--- spec/conf.js
+++ spec/conf.js
@@ -5,6 +5,10 @@
 
 exports.config = {
     framework: 'mocha',
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 10 * 1000
+    },
     sauceUser: process.env.SAUCE_USER,
     sauceKey: process.env.SAUCE_KEY,
     specs: [ 'test.js' ]
